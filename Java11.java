15ad1b855c93ce4598e89dd879c8e846a40ce291
savita <savitapawar1511@gmail.com>
2024-10-18 13:59:58 +0530

    add new file

---
+++ Java11.java
@@ -0,0 +1,8 @@
+public class Demo{
+    public static void main(String args[]){
+        System.out.println("Hello Technosignia");
+        System.out.println("Hello world");
+        System.out.println("Hello Jenkins");
+
+    }
+}
